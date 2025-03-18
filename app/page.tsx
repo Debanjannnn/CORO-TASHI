2dfdf6892cd4a225db18c7a2743fe784b4e3477a
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2025-03-18 20:07:21 +0530

    bento

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 
+import { CardHoverEffectDemo } from "@/components/CardHover";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import { HeroScrollDemo } from "@/components/HeroScroll";
 import FloatingNavDemo from "@/components/Navbar";
 
@@ -7,8 +10,10 @@ export default function Home() {
   return (
     <div className="bg-black">
        <FloatingNavDemo/>
-      
        <HeroScrollDemo/>
+       {/* < CardHoverEffectDemo/> */}
+       <Features/>
+       <Footer/>
     </div>
    
   )}
--- components/HeroScroll.tsx
+++ components/HeroScroll.tsx
@@ -15,7 +15,7 @@ export function HeroScrollDemo() {
             <h1 className="text-4xl font-semibold text-black dark:text-white">
               Unleash the power of <br />
               <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                CORE <span className="text-orange-500">DAO</span>
+                CORE <span className="text-orange-500 ">DAO</span>
               </span>
             </h1>
           </>
