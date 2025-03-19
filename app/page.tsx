7b6d6faa74993a74429349309eb3ba1af0243f51
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2025-03-19 12:33:08 +0530

    partial landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 
 import { CardHoverEffectDemo } from "@/components/CardHover";
+import { CoreStrengths } from "@/components/CoreStrengths";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import { HeroScrollDemo } from "@/components/HeroScroll";
 import FloatingNavDemo from "@/components/Navbar";
+import Typeform from "@/components/Typeform";
 import { FeaturesSection } from "@/components/VoFeatures";
 
 
@@ -12,9 +14,10 @@ export default function Home() {
     <div className="bg-black">
        <FloatingNavDemo/>
        <HeroScrollDemo/>
-       {/* < CardHoverEffectDemo/> */}
+       <CoreStrengths/>
        <FeaturesSection/>
        {/* <Features/> */}
+       <Typeform/>
        <Footer/>
     </div>
    
